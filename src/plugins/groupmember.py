5e3146850bb3748209f08caf0125a1bb3ef021c7
InEase <InEase28@gmail.com>
2022-08-14 18:26:32 +0800

    feat: upload groud info file

--- src/plugins/groupmember.py
+++ src/plugins/groupmember.py
@@ -1,11 +1,17 @@
-from nonebot import get_bot, on_command
+import time
+from pathlib import Path
+from typing import Union
+
+import pandas as pd
+from gocqapi import api
+from nonebot import on_command
 from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, PrivateMessageEvent
 from nonebot.matcher import Matcher
 from nonebot.params import ArgPlainText, CommandArg
 from nonebot.permission import SUPERUSER
-from nonebot.rule import to_me
 
-member_list = on_command("群列表", rule=to_me(), priority=5, permission=SUPERUSER)
+member_list = on_command("群列表", priority=5, permission=SUPERUSER)
 
 
 @member_list.handle()
@@ -16,15 +22,18 @@ async def handle_first_receive(matcher: Matcher, args: Message = CommandArg()):
 
 
 @member_list.got("group_id", prompt="你想查询哪个群呢？")
-async def handle_city(city_name: str = ArgPlainText("group_id")):
-    members = await get_members(city_name)
-    await member_list.finish(members)
+async def handle_city(event: Union[PrivateMessageEvent, GroupMessageEvent], city_name: str = ArgPlainText("group_id")):
+    file = await get_members(city_name)
+    await api.upload_group_file(event.group_id, file, str(file), None)
+    await member_list.finish("执行成功")
 
 
-# 在这里编写获取天气信息的函数
-async def get_members(group_id: str) -> str:
-    bot = get_bot()
-    members = await bot.call_api('get_group_member_list', group_id=group_id)
-    # extract card from members list
-    card_list = [member["card"] for member in members]
-    return "\n".join(card_list)
+async def get_members(group_id: str) -> Path:
+    members = await api.get_group_member_list(group_id)
+    members_df = pd.DataFrame(members)
+    # group_id date member_list
+    date = time.strftime("%Y-%m-%d", time.localtime())
+    file_name = Path(f"群成员列表 {date} {group_id}.xlsx")
+    # to xlsx
+    members_df.to_excel(file_name, index=False)
+    return file_name
